02bdc39a7b376807e719d3a29c7c4b9771195052
DeadPackets <youssef@ctf.ae>
2026-08-08 17:57:17 +0000

    fix(sqli): prepared statements on both backends at every level

--- vulnerabilities/sqli/source/high.php
+++ vulnerabilities/sqli/source/high.php
@@ -4,38 +4,19 @@ if( isset( $_SESSION [ 'id' ] ) ) {
 	// Get input
 	$id = $_SESSION[ 'id' ];
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id' LIMIT 1;";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"], $query ) or die( '<pre>Something went wrong.</pre>' );
+	// Was a number entered?
+	if( is_numeric( $id ) ) {
+		$id = intval( $id );
 
-			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
-				// Get values
-				$first = $row["first_name"];
-				$last  = $row["last_name"];
+		switch ($_DVWA['SQLI_DB']) {
+			case MYSQL:
+				// Check database
+				$data = $db->prepare( 'SELECT first_name, last_name FROM users WHERE user_id = (:id) LIMIT 1;' );
+				$data->bindParam( ':id', $id, PDO::PARAM_INT );
+				$data->execute();
 
-				// Feedback for end user
-				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-			}
-
-			((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);		
-			break;
-		case SQLITE:
-			global $sqlite_db_connection;
-
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id' LIMIT 1;";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
-
-			if ($results) {
-				while ($row = $results->fetchArray()) {
+				// Get results
+				while( $row = $data->fetch() ) {
 					// Get values
 					$first = $row["first_name"];
 					$last  = $row["last_name"];
@@ -43,10 +24,34 @@ if( isset( $_SESSION [ 'id' ] ) ) {
 					// Feedback for end user
 					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
-			}
-			break;
+
+				break;
+			case SQLITE:
+				global $sqlite_db_connection;
+
+				try {
+					$stmt = $sqlite_db_connection->prepare( 'SELECT first_name, last_name FROM users WHERE user_id = :id LIMIT 1;' );
+					$stmt->bindValue( ':id', $id, SQLITE3_INTEGER );
+					$results = $stmt->execute();
+				} catch (Exception $e) {
+					echo 'Caught exception: ' . $e->getMessage();
+					exit();
+				}
+
+				if ($results) {
+					while ($row = $results->fetchArray()) {
+						// Get values
+						$first = $row["first_name"];
+						$last  = $row["last_name"];
+
+						// Feedback for end user
+						$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+					}
+				} else {
+					echo "Error in fetch ".$sqlite_db->lastErrorMsg();
+				}
+				break;
+		}
 	}
 }
 
