9d57eaedcf3a87ee8cd9c21580ca639ed909ca9d
overscr <gatingk@qodia.net>
2026-08-08 18:15:36 -0400

    Belt-and-braces harden command injection: escapeshellarg + encode ping output
    The octet validation already prevents any shell metacharacter from reaching
    shell_exec(), but wrap the target in escapeshellarg() as well so the command
    construction is safe even if that validation were ever loosened. Also encode
    the ping output before printing it, since it is still text written into the
    page.

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -13,18 +13,20 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 		// If all 4 octets are int's put the IP back together.
 		$target = $octet[0] . '.' . $octet[1] . '.' . $octet[2] . '.' . $octet[3];
 
-		// Determine OS and execute the ping command.
+		// Determine OS and execute the ping command. The target is passed as a
+		// single escaped shell argument, on top of the octet validation above,
+		// so it can never be interpreted as shell syntax.
 		if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
 			// Windows
-			$cmd = shell_exec( 'ping  ' . $target );
+			$cmd = shell_exec( 'ping  ' . escapeshellarg( $target ) );
 		}
 		else {
 			// *nix
-			$cmd = shell_exec( 'ping  -c 4 ' . $target );
+			$cmd = shell_exec( 'ping  -c 4 ' . escapeshellarg( $target ) );
 		}
 
 		// Feedback for the end user
-		$html .= "<pre>{$cmd}</pre>";
+		$html .= "<pre>" . htmlspecialchars( $cmd, ENT_QUOTES, 'UTF-8' ) . "</pre>";
 	}
 	else {
 		// Oops. Let the user know theres a mistake
--- vulnerabilities/exec/source/medium.php
+++ vulnerabilities/exec/source/medium.php
@@ -12,18 +12,20 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 	if( ( sizeof( $octet ) == 4 ) && ( is_numeric( $octet[0] ) ) && ( is_numeric( $octet[1] ) ) && ( is_numeric( $octet[2] ) ) && ( is_numeric( $octet[3] ) ) ) {
 		$target = $octet[0] . '.' . $octet[1] . '.' . $octet[2] . '.' . $octet[3];
 
-		// Determine OS and execute the ping command.
+		// Determine OS and execute the ping command. The target is passed as a
+		// single escaped shell argument, on top of the octet validation above,
+		// so it can never be interpreted as shell syntax.
 		if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
 			// Windows
-			$cmd = shell_exec( 'ping  ' . $target );
+			$cmd = shell_exec( 'ping  ' . escapeshellarg( $target ) );
 		}
 		else {
 			// *nix
-			$cmd = shell_exec( 'ping  -c 4 ' . $target );
+			$cmd = shell_exec( 'ping  -c 4 ' . escapeshellarg( $target ) );
 		}
 
 		// Feedback for the end user
-		$html .= "<pre>{$cmd}</pre>";
+		$html .= "<pre>" . htmlspecialchars( $cmd, ENT_QUOTES, 'UTF-8' ) . "</pre>";
 	}
 	else {
 		// Oops. Let the user know theres a mistake
