fee80d5bb0885ac196b9044aa4a38fdf1d9fa0a5
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 02:37:46 +0300

    Require-CSRF-tokens-and-randomize-session-identifiers
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- vulnerabilities/csrf/source/low.php
+++ vulnerabilities/csrf/source/low.php
@@ -1,6 +1,7 @@
 <?php
 
 if( isset( $_GET[ 'Change' ] ) ) {
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
 	// Get input
 	$pass_new  = $_GET[ 'password_new' ];
 	$pass_conf = $_GET[ 'password_conf' ];
@@ -26,5 +27,7 @@ if( isset( $_GET[ 'Change' ] ) ) {
 
 	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 }
+
+generateSessionToken();
 
 ?>
--- vulnerabilities/csrf/source/medium.php
+++ vulnerabilities/csrf/source/medium.php
@@ -1,6 +1,7 @@
 <?php
 
 if( isset( $_GET[ 'Change' ] ) ) {
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
 	// Checks to see where the request came from
 	if( stripos( $_SERVER[ 'HTTP_REFERER' ] ,$_SERVER[ 'SERVER_NAME' ]) !== false ) {
 		// Get input
@@ -33,5 +34,7 @@ if( isset( $_GET[ 'Change' ] ) ) {
 
 	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 }
+
+generateSessionToken();
 
 ?>
