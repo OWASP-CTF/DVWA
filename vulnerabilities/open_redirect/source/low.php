26c28598a6594fb67d79da18a85014e4d89df4ed
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 07:20:29 +0900

    fix(open-redirect): restrict low redirects to info page

--- vulnerabilities/open_redirect/source/low.php
+++ vulnerabilities/open_redirect/source/low.php
@@ -1,7 +1,32 @@
 <?php
 
-if (array_key_exists ("redirect", $_GET) && $_GET['redirect'] != "") {
-	header ("location: " . $_GET['redirect']);
+if (array_key_exists ("redirect", $_GET) && is_string ($_GET['redirect']) && $_GET['redirect'] != "") {
+	$redirect = parse_url ($_GET['redirect']);
+
+	if ($redirect !== false &&
+		!array_key_exists ("scheme", $redirect) &&
+		!array_key_exists ("host", $redirect) &&
+		!array_key_exists ("user", $redirect) &&
+		!array_key_exists ("pass", $redirect) &&
+		!array_key_exists ("port", $redirect) &&
+		array_key_exists ("path", $redirect) &&
+		$redirect['path'] === "info.php") {
+		$target = "info.php";
+		if (array_key_exists ("query", $redirect)) {
+			$target .= "?" . $redirect['query'];
+		}
+		if (array_key_exists ("fragment", $redirect)) {
+			$target .= "#" . $redirect['fragment'];
+		}
+
+		header ("location: " . $target);
+		exit;
+	}
+
+	http_response_code (500);
+	?>
+	<p>You can only redirect to the info page.</p>
+	<?php
 	exit;
 }
 
