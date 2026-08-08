7f47e8ccd1fd6bd9cb476cf882322b4276a5720d
DeadPackets <youssef@ctf.ae>
2026-08-08 17:41:27 +0000

    fix(weak_id, open_redirect): CSPRNG session cookies and an allowlisted redirect target
    weak_id low/medium/high derived dvwaSession from a counter, time() or md5 of a
    counter, all trivially predictable. All three now use bin2hex(random_bytes(20))
    with the same cookie flags as impossible.php.
    
    open_redirect low redirected anywhere, medium only blocked literal http(s)://
    and high accepted any string containing "info.php". All three now require the
    target to match ^info\.php\?id=\d+$.

--- vulnerabilities/open_redirect/source/high.php
+++ vulnerabilities/open_redirect/source/high.php
@@ -1,7 +1,7 @@
 <?php
 
 if (array_key_exists ("redirect", $_GET) && $_GET['redirect'] != "") {
-	if (strpos($_GET['redirect'], "info.php") !== false) {
+	if (preg_match ('/^info\.php\?id=\d+$/', $_GET['redirect'])) {
 		header ("location: " . $_GET['redirect']);
 		exit;
 	} else {
--- vulnerabilities/open_redirect/source/medium.php
+++ vulnerabilities/open_redirect/source/medium.php
@@ -7,8 +7,14 @@ if (array_key_exists ("redirect", $_GET) && $_GET['redirect'] != "") {
 		<p>Absolute URLs not allowed.</p>
 		<?php
 		exit;
-	} else {
+	} else if (preg_match ('/^info\.php\?id=\d+$/', $_GET['redirect'])) {
 		header ("location: " . $_GET['redirect']);
+		exit;
+	} else {
+		http_response_code (500);
+		?>
+		<p>Unknown redirect target.</p>
+		<?php
 		exit;
 	}
 }
