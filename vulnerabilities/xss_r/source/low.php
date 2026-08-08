a56f38d0b29153b2c549abd42f88cdb6004fb7dc
overscr <gatingk@qodia.net>
2026-08-08 18:54:15 -0400

    fix(xss_r): HTML-encode the reflected name instead of blacklisting tags

--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -1,11 +1,13 @@
 <?php
 
-header ("X-XSS-Protection: 0");
-
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	// Encode the value for the HTML context it is echoed into, so it can only
+	// ever render as text.
+	$name = htmlspecialchars( $_GET[ 'name' ], ENT_QUOTES, 'UTF-8' );
+
 	// Feedback for end user
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
+	$html .= "<pre>Hello {$name}</pre>";
 }
 
 ?>
--- vulnerabilities/xss_r/source/medium.php
+++ vulnerabilities/xss_r/source/medium.php
@@ -1,11 +1,10 @@
 <?php
 
-header ("X-XSS-Protection: 0");
-
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
-	// Get input
-	$name = str_replace( '<script>', '', $_GET[ 'name' ] );
+	// Stripping the literal string "<script>" left every other tag and event
+	// handler intact. Encode for the output context instead of blacklisting.
+	$name = htmlspecialchars( $_GET[ 'name' ], ENT_QUOTES, 'UTF-8' );
 
 	// Feedback for end user
 	$html .= "<pre>Hello {$name}</pre>";
