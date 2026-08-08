c636c3cf471d3f81ced8eefe379528f09207137b
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 01:47:44 +0900

    Fix command injection in Medium ping challenge
    Validate the submitted target as an IP address before constructing the ping command, preventing shell metacharacters and arbitrary commands from reaching shell_exec.

--- vulnerabilities/exec/source/medium.php
+++ vulnerabilities/exec/source/medium.php
@@ -2,29 +2,26 @@
 
 if( isset( $_POST[ 'Submit' ]  ) ) {
 	// Get input
-	$target = $_REQUEST[ 'ip' ];
+	$target = trim( $_REQUEST[ 'ip' ] );
 
-	// Set blacklist
-	$substitutions = array(
-		'&&' => '',
-		';'  => '',
-	);
+	// Only permit valid IP addresses to reach the shell command.
+	if( filter_var( $target, FILTER_VALIDATE_IP ) ) {
+		// Determine OS and execute the ping command.
+		if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+			// Windows
+			$cmd = shell_exec( 'ping  ' . $target );
+		}
+		else {
+			// *nix
+			$cmd = shell_exec( 'ping  -c 4 ' . $target );
+		}
 
-	// Remove any of the characters in the array (blacklist).
-	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
-
-	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
+		// Feedback for the end user
+		$html .= "<pre>{$cmd}</pre>";
 	}
 	else {
-		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+		$html .= '<pre>ERROR: You have entered an invalid IP.</pre>';
 	}
-
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
 }
 
 ?>
