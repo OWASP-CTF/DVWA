aae9378bcd0fac4bc3c89296959a104cef8d8f98
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 02:10:49 +0900

    Fix API user update privilege escalation
    Restrict user updates to the declared name field so crafted requests cannot overwrite the authorization level.

--- vulnerabilities/api/src/UserController.php
+++ vulnerabilities/api/src/UserController.php
@@ -221,9 +221,6 @@ class UserController
 		if (array_key_exists ("name", $input)) {
 			$this->data[$id]->name = $input['name'];
 		}
-		if (array_key_exists ("level", $input)) {
-			$this->data[$id]->level = intval ($input['level']);
-		}
 		$response['status_code_header'] = 'HTTP/1.1 200 OK';
 		$response['body'] = json_encode ($this->data[$id]->toArray($this->version));
 		return $response;
