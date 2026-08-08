79f300634714d1cee3a2c5a076c0fc8ddce7c231
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 02:26:41 +0900

    fix(upload): prevent executable polyglots at high security
    Validate the server-detected image type, re-encode accepted JPEG and PNG files, and store them under random server-generated filenames so attacker-controlled names and embedded payloads are not retained.

--- vulnerabilities/upload/source/high.php
+++ vulnerabilities/upload/source/high.php
@@ -1,28 +1,39 @@
 <?php
 
 if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
-
 	// File information
-	$uploaded_name = $_FILES[ 'uploaded' ][ 'name' ];
-	$uploaded_ext  = substr( $uploaded_name, strrpos( $uploaded_name, '.' ) + 1);
 	$uploaded_size = $_FILES[ 'uploaded' ][ 'size' ];
 	$uploaded_tmp  = $_FILES[ 'uploaded' ][ 'tmp_name' ];
+	$image_info    = getimagesize( $uploaded_tmp );
 
 	// Is it an image?
-	if( ( strtolower( $uploaded_ext ) == "jpg" || strtolower( $uploaded_ext ) == "jpeg" || strtolower( $uploaded_ext ) == "png" ) &&
-		( $uploaded_size < 100000 ) &&
-		getimagesize( $uploaded_tmp ) ) {
+	if( $uploaded_size < 100000 && $image_info &&
+		( $image_info[2] == IMAGETYPE_JPEG || $image_info[2] == IMAGETYPE_PNG ) ) {
 
-		// Can we move the file to the upload folder?
-		if( !move_uploaded_file( $uploaded_tmp, $target_path ) ) {
-			// No
+		// Decode and re-encode the image so embedded content is not retained.
+		if( $image_info[2] == IMAGETYPE_JPEG ) {
+			$image = imagecreatefromjpeg( $uploaded_tmp );
+			$uploaded_ext = 'jpg';
+		}
+		else {
+			$image = imagecreatefrompng( $uploaded_tmp );
+			$uploaded_ext = 'png';
+		}
+
+		$target_path  = DVWA_WEB_PAGE_TO_ROOT . 'hackable/uploads/';
+		$target_path .= bin2hex( random_bytes( 16 ) ) . '.' . $uploaded_ext;
+		$uploaded = $image && ( $uploaded_ext == 'jpg' ?
+			imagejpeg( $image, $target_path, 100 ) :
+			imagepng( $image, $target_path, 9 ) );
+
+		if( $image ) {
+			imagedestroy( $image );
+		}
+
+		if( !$uploaded ) {
 			$html .= '<pre>Your image was not uploaded.</pre>';
 		}
 		else {
-			// Yes!
 			$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
 		}
 	}
