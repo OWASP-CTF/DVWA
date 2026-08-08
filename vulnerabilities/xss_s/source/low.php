f2db3052c331fb25883a642d029f635bed445400
Gabriel Seet <gabeseet@gmail.com>
2026-08-08 06:29:24 -0700

    experiment: revert group A2 (sqli, sqli_blind, exec, fi)

--- vulnerabilities/xss_s/source/low.php
+++ vulnerabilities/xss_s/source/low.php
@@ -7,16 +7,18 @@ if( isset( $_POST[ 'btnSign' ] ) ) {
 
 	// Sanitize message input
 	$message = stripslashes( $message );
-	$message = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $message ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+	$message = htmlspecialchars( $message, ENT_QUOTES, 'UTF-8' );
 
 	// Sanitize name input
-	$name = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $name ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+	$name = stripslashes( $name );
+	$name = htmlspecialchars( $name, ENT_QUOTES, 'UTF-8' );
 
-	// Update database
-	$query  = "INSERT INTO guestbook ( comment, name ) VALUES ( '$message', '$name' );";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
-
-	//mysql_close();
+	// Update database, using a prepared statement so the input can never be
+	// parsed as SQL.
+	$data = $db->prepare( 'INSERT INTO guestbook ( comment, name ) VALUES ( :message, :name );' );
+	$data->bindParam( ':message', $message, PDO::PARAM_STR );
+	$data->bindParam( ':name', $name, PDO::PARAM_STR );
+	$data->execute();
 }
 
 ?>
