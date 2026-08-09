56ec6d208de794bb4b9044dd94dd9ad44f82a7c7
overscr <gatingk@qodia.net>
2026-08-08 23:38:15 -0400

    exec: validate ping target as a numeric IP octet sequence

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -1,21 +1,46 @@
 <?php
 
 if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Get input
-	$target = $_REQUEST[ 'ip' ];
+	// Check Anti-CSRF token
+	if (array_key_exists ("session_token", $_SESSION)) {
+		$session_token = $_SESSION[ 'session_token' ];
+	} else {
+		$session_token = "";
+	}
+	checkToken( $_REQUEST[ 'user_token' ], $session_token, 'index.php' );
 
-	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
+	// Get input
+	$target = trim( $_REQUEST[ 'ip' ] );
+
+	// Split the IP into 4 octets
+	$octet = explode( ".", $target );
+
+	// Only proceed if each octet is a plain number - this stops any shell
+	// metacharacters from ever reaching shell_exec().
+	if( ( sizeof( $octet ) == 4 ) && ( is_numeric( $octet[0] ) ) && ( is_numeric( $octet[1] ) ) && ( is_numeric( $octet[2] ) ) && ( is_numeric( $octet[3] ) ) ) {
+		// If all 4 octets are int's put the IP back together.
+		$target = $octet[0] . '.' . $octet[1] . '.' . $octet[2] . '.' . $octet[3];
+
+		// Determine OS and execute the ping command.
+		if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+			// Windows
+			$cmd = shell_exec( 'ping  ' . $target );
+		}
+		else {
+			// *nix
+			$cmd = shell_exec( 'ping  -c 4 ' . $target );
+		}
+
+		// Feedback for the end user
+		$html .= "<pre>{$cmd}</pre>";
 	}
 	else {
-		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+		// Oops. Let the user know theres a mistake
+		$html .= '<pre>ERROR: You have entered an invalid IP.</pre>';
 	}
-
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
 }
+
+// Generate Anti-CSRF token
+generateSessionToken();
 
 ?>
