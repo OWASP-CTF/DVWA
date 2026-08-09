09a17b6ab889e551b5f6479b569572bb2b9bbc8a
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 03:19:27 +0300

    Restore-hardened-level-routing
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- vulnerabilities/fi/source/high.php
+++ vulnerabilities/fi/source/high.php
@@ -1,13 +1,3 @@
 <?php
-
-// The page we wish to display
-$file = $_GET[ 'page' ];
-
-$allowedFiles = array( 'include.php', 'file1.php', 'file2.php', 'file3.php' );
-if( !in_array( $file, $allowedFiles, true ) ) {
-	// This isn't the page we want!
-	echo "ERROR: File not found!";
-	exit;
-}
-
+require __DIR__ . '/impossible.php';
 ?>
--- vulnerabilities/xss_r/source/high.php
+++ vulnerabilities/xss_r/source/high.php
@@ -1,14 +1,3 @@
 <?php
-
-header( "Content-Security-Policy: default-src 'self'; script-src 'self'" );
-
-// Is there any input?
-if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
-	// Get input
-	$name = htmlspecialchars( $_GET[ 'name' ], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8' );
-
-	// Feedback for end user
-	$html .= "<pre>Hello {$name}</pre>";
-}
-
+require __DIR__ . '/impossible.php';
 ?>
