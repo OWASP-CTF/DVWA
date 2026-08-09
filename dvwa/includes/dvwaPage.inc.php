40f75e9369f56d3f5bd63969d66ecec2b05d60d3
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 09:51:58 +0300

    Revert zero-gain guestbook and reference-level candidates
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- dvwa/includes/dvwaPage.inc.php
+++ dvwa/includes/dvwaPage.inc.php
@@ -614,8 +614,14 @@ function dvwaGuestbook() {
 	$guestbook = '';
 
 	while( $row = mysqli_fetch_row( $result ) ) {
-		$name    = htmlspecialchars( $row[0], ENT_QUOTES, 'UTF-8' );
-		$comment = htmlspecialchars( $row[1], ENT_QUOTES, 'UTF-8' );
+		if( dvwaSecurityLevelGet() == 'impossible' ) {
+			$name    = htmlspecialchars( $row[0] );
+			$comment = htmlspecialchars( $row[1] );
+		}
+		else {
+			$name    = $row[0];
+			$comment = $row[1];
+		}
 
 		$guestbook .= "<div id=\"guestbook_comments\">Name: {$name}<br />" . "Message: {$comment}<br /></div>\n";
 	}
