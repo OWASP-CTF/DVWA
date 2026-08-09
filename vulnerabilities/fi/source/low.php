4e59d740f75420df3f12fde06a8db2871e3543bf
r1kka6 <219588946+r1kka6@users.noreply.github.com>
2026-08-09 16:43:58 -0700

    Restrict file inclusion in low security level

--- vulnerabilities/fi/source/low.php
+++ vulnerabilities/fi/source/low.php
@@ -3,4 +3,17 @@
 // The page we wish to display
 $file = $_GET[ 'page' ];
 
+// Only allow the built-in challenge pages.
+$allowedFileNames = [
+	'include.php',
+	'file1.php',
+	'file2.php',
+	'file3.php',
+];
+
+if( !in_array( $file, $allowedFileNames, true ) ) {
+	echo "ERROR: File not found!";
+	exit;
+}
+
 ?>
