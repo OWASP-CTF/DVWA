2c3c23d59e2a8e456759a05cd7f22611a100350f
r1kka6 <219588946+r1kka6@users.noreply.github.com>
2026-08-09 16:47:37 -0700

    Restrict DOM XSS language selection in low security level

--- vulnerabilities/xss_d/source/low.php
+++ vulnerabilities/xss_d/source/low.php
@@ -1,5 +1,17 @@
 <?php
 
-# No protections, anything goes
+if( array_key_exists( 'default', $_GET ) && !is_null( $_GET[ 'default' ] ) ) {
+	$allowedLanguages = [
+		'English',
+		'French',
+		'Spanish',
+		'German',
+	];
+
+	if( !in_array( $_GET[ 'default' ], $allowedLanguages, true ) ) {
+		header( 'Location: ?default=English' );
+		exit;
+	}
+}
 
 ?>
