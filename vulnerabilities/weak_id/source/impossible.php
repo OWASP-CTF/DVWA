c1433ac838986432b9313eb9139089b4232af2bd
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 10:07:39 +0300

    Respect HTTPS when setting weak ID cookies
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- vulnerabilities/weak_id/source/impossible.php
+++ vulnerabilities/weak_id/source/impossible.php
@@ -1,9 +1,13 @@
 <?php
 
+function weakIdCookieSecure() {
+	return !empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off';
+}
+
 $html = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$cookie_value = bin2hex(random_bytes(20));
-	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], true, true);
+	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], weakIdCookieSecure(), true);
 }
 ?>
