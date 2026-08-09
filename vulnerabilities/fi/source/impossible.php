0ac1f4d7a6d58a704bdec89fd5957a3e84c7cfaf
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 10:23:26 +0300

    Revert zero-gain File Inclusion reachability candidate
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- vulnerabilities/fi/source/impossible.php
+++ vulnerabilities/fi/source/impossible.php
@@ -1,12 +1,5 @@
 <?php
 
-// No page requested. Leave $file unset so index.php can send the browser to
-// the module's landing page, exactly as it does for the unhardened levels.
-// Erroring out here made /vulnerabilities/fi/ unreachable.
-if( !array_key_exists( 'page', $_GET ) ) {
-	return;
-}
-
 // The page we wish to display
 $file = $_GET[ 'page' ];
 
@@ -18,7 +11,7 @@ $configFileNames = [
     'file3.php',
 ];
 
-if( !is_string($file) || !in_array($file, $configFileNames, true) ) {
+if( !in_array($file, $configFileNames) ) {
     // This isn't the page we want!
     echo "ERROR: File not found!";
     exit;
