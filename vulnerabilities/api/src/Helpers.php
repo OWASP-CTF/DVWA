17739dc63bece3b7eee3a66c86f4b43337af479e
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 10:53:55 +0300

    fix(api): accept JSON media-type parameters
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- vulnerabilities/api/src/Helpers.php
+++ vulnerabilities/api/src/Helpers.php
@@ -4,7 +4,9 @@ namespace Src;
 
 class Helpers {
 	public static function check_content_type() {
-		if (array_key_exists ("CONTENT_TYPE", $_SERVER) && $_SERVER['CONTENT_TYPE'] == "application/json") {
+		$contentType = $_SERVER['CONTENT_TYPE'] ?? '';
+		$mediaType = strtolower(trim(explode(';', $contentType, 2)[0]));
+		if ($mediaType === "application/json") {
 			return true;
 		} else {
 			$response['status_code_header'] = 'HTTP/1.1 415 Unsupported Media Type';
