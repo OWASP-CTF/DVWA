5fcd8da81af344de8e9849f6d7e5e2afd94dee57
Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 07:01:52 +0300

    Restrict cross-origin API access
    Signed-off-by: Riki-Lansilahti <7629042+lansiri@users.noreply.github.com>

--- vulnerabilities/api/public/index.php
+++ vulnerabilities/api/public/index.php
@@ -8,11 +8,7 @@ use Src\OrderController;
 use Src\LoginController;
 use Src\Helpers;
 
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
